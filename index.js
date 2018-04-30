c3df4a8ac5069bd7f8904f826a62af518275a488
venondev <venondesign@gmail.com>
2018-04-30 10:58:29 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import backgroundSetup from './lib/backgroundSetup';
+import contentSetup from './lib/contentSetup';
+
+export const setupMessageListener = backgroundSetup;
+export const initBGFunctions = contentSetup;
+
+
+
